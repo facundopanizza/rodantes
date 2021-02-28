e90bea8a46ebae47d1d079ba8ba3c2fa57c3298f
Facundo Panizza <facundo@panizza.dev>
2021-02-28 08:16:05 -0300

    Bigger modal for photos

--- resources/views/components/photo.blade.php
+++ resources/views/components/photo.blade.php
@@ -1,5 +1,5 @@
 <div class="modal" id="photo{{ $key }}">
-    <div class="modal-dialog modal-dialog-centered">
-        <img class="img-fluid" style="" src="{{ asset($link) }}">
+    <div class="modal-dialog modal-xl modal-dialog-centered">
+        <img class="img-fluid mx-auto" style="" src="{{ asset($link) }}">
     </div>
 </div>
